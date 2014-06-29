36700d39f8be36b3befdd299d5731a93f5f3889e
Steven Skoczen <steven@quantumimagery.com>
2014-06-28 20:35:15 -0700

    Adds a few more screenshots to the site.  Love it.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A chrome extention to beautifully interrupt mindless browsing, and put your time back in your hands.
+
+
+Learn more and install from http://mindfulbrowsing.org
--- extension/js/photoInfo.js
+++ extension/js/photoInfo.js
@@ -1,56 +1,5 @@
-(function () {
-    var photoInfo = {
-        "photos": [{
-        //     "url": "http://mindfulbrowsing.org/photos/d8b4e6371a19bb2919e6c66e89b4ff8e573319ce.jpg",
-        //     "credit": "Chris Gin",
-        //     "start_date_human": "Jun 28 2014",
-        //     "start_date": 1404002629000,
-        //     "credit_url": "http://chrisgin.com"
-        // }, {
-        //     "url": "http://mindfulbrowsing.org/photos/bec87912d47aea03e4ef7a13583df6b732ab1604.jpg",
-        //     "credit": "Chris Gin",
-        //     "start_date_human": "Jun 30 2014",
-        //     "start_date": 1404175429000,
-        //     "credit_url": "http://chrisgin.com"
-        // }, {
-        //     "url": "http://mindfulbrowsing.org/photos/0666077a3ed4f907050381c5c829b9cd07e118aa.jpg",
-        //     "credit": "Chris Gin",
-        //     "start_date_human": "Jul 02 2014",
-        //     "start_date": 1404348229000,
-        //     "credit_url": "http://chrisgin.com"
-        // }, {
-            "url": "http://mindfulbrowsing.org/photos/5aac58fd2f79ca4f37dba3dc2be144d7a9e34398.jpg",
-            "credit": "Chris Gin",
-            "start_date_human": "Jul 04 2014",
-            "start_date": 1404521029000,
-            "credit_url": "http://chrisgin.com"
-        }, {
-            "url": "http://mindfulbrowsing.org/photos/4faa4c07de8e114c595c4cac6939e72c639e36ba.jpg",
-            "credit": "Chris Gin",
-            "start_date_human": "Jul 06 2014",
-            "start_date": 1404693829000,
-            "credit_url": "http://chrisgin.com"
-        }, {
-            "url": "http://mindfulbrowsing.org/photos/b0622045377f8b22ee0f8d15dd9b68c145892462.jpg",
-            "credit": "Chris Gin",
-            "start_date_human": "Jul 08 2014",
-            "start_date": 1404866629000,
-            "credit_url": "http://chrisgin.com"
-        }, {
-            "url": "http://mindfulbrowsing.org/photos/ad666db13f0e1392f3670b0de333605617df9cfd.jpg",
-            "credit": "Chris Gin",
-            "start_date_human": "Jul 10 2014",
-            "start_date": 1405039429000,
-            "credit_url": "http://chrisgin.com"
-        }, {
-            "url": "http://mindfulbrowsing.org/photos/afbe05b6074c8d1abde40fc4f9d1845fc89b604b.jpg",
-            "credit": "Chris Gin",
-            "start_date_human": "Jul 12 2014",
-            "start_date": 1405212229000,
-            "credit_url": "http://chrisgin.com"
-        }],
-        "last_update": 1404005217000
-    };
+(function() {
+    var photoInfo = {"photos": [{"url": "http://mindfulbrowsing.org/photos/d8b4e6371a19bb2919e6c66e89b4ff8e573319ce.jpg", "credit": "Chris Gin", "start_date_human": "Jun 28 2014", "start_date": 1404002629000, "credit_url": "http://chrisgin.com"}, {"url": "http://mindfulbrowsing.org/photos/bec87912d47aea03e4ef7a13583df6b732ab1604.jpg", "credit": "Chris Gin", "start_date_human": "Jun 30 2014", "start_date": 1404175429000, "credit_url": "http://chrisgin.com"}, {"url": "http://mindfulbrowsing.org/photos/0666077a3ed4f907050381c5c829b9cd07e118aa.jpg", "credit": "Chris Gin", "start_date_human": "Jul 02 2014", "start_date": 1404348229000, "credit_url": "http://chrisgin.com"}, {"url": "http://mindfulbrowsing.org/photos/5aac58fd2f79ca4f37dba3dc2be144d7a9e34398.jpg", "credit": "Chris Gin", "start_date_human": "Jul 04 2014", "start_date": 1404521029000, "credit_url": "http://chrisgin.com"}, {"url": "http://mindfulbrowsing.org/photos/4faa4c07de8e114c595c4cac6939e72c639e36ba.jpg", "credit": "Chris Gin", "start_date_human": "Jul 06 2014", "start_date": 1404693829000, "credit_url": "http://chrisgin.com"}, {"url": "http://mindfulbrowsing.org/photos/b0622045377f8b22ee0f8d15dd9b68c145892462.jpg", "credit": "Chris Gin", "start_date_human": "Jul 08 2014", "start_date": 1404866629000, "credit_url": "http://chrisgin.com"}, {"url": "http://mindfulbrowsing.org/photos/ad666db13f0e1392f3670b0de333605617df9cfd.jpg", "credit": "Chris Gin", "start_date_human": "Jul 10 2014", "start_date": 1405039429000, "credit_url": "http://chrisgin.com"}, {"url": "http://mindfulbrowsing.org/photos/afbe05b6074c8d1abde40fc4f9d1845fc89b604b.jpg", "credit": "Chris Gin", "start_date_human": "Jul 12 2014", "start_date": 1405212229000, "credit_url": "http://chrisgin.com"}], "last_update": 1404012883000};
     window.mindfulBrowsing = window.mindfulBrowsing || {};
     window.mindfulBrowsing.photoInfo = photoInfo;
 })();
